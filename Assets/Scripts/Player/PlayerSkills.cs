ae3f563a9e7e93a9cfcdf863415c39c68840b556
Joseajierro <jrmar0805@gmail.com>
2025-08-25 21:20:19 +0200

    player skills

---
+++ Assets/Scripts/Player/PlayerSkills.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerSkills : MonoBehaviour
+    {
+        public bool moreLife = false;
+        public bool moreDamage = false;
+        public bool invulnerableWhenDash = false;
+        public bool doubleShoot;
+        public bool holyMantle;
+    }
+}
