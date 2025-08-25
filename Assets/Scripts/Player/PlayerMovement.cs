5e03cd45e8303fceb2e89da622fda05eaf6bd815
Joseajierro <jrmar0805@gmail.com>
2025-08-25 18:43:50 +0200

    Migration to unity 2022

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        [Header("Player Movement")]
+        private float speed;
+        
+        private Vector2 moveDirection;
+        private Rigidbody2D rb2d;
+
+        private void Awake()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            InputsValues();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void InputsValues()
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+            
+            moveDirection = new Vector2(x, y).normalized;
+        }
+
+        private void Move()
+        {
+            rb2d.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        }
+
+        public Vector2 GetDirection()
+        {
+            return  moveDirection;
+        }
+    }
+}
